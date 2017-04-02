47a7fae6a33319d57b9fef9bbfe3b164f46c1ef4
sarmadsaleem <sarmad.saleem@live.com>
2017-04-02 23:08:24 +0200

    Timeline added, tab removed

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,7 +28,7 @@ $.getJSON(incidents_endpoint, function(data) {
 	$(data.feed.entry).each(function(){
 
 		year = this.gsx$yearofincident.$t.split(' ');
-		year = year[year.length - 1][0] + year[year.length - 1][1] + '<br>' + year[year.length - 1][2] + year[year.length - 1][3];
+		year = year[year.length - 1][0] + year[year.length - 1][1] + year[year.length - 1][2] + year[year.length - 1][3];
 		$('#inci-con').append('<div id="i-anc-'+counter+'" class="col-xs-3 col-md-2 date" div-toggle="#incident-'+counter+'"><div>'+year+'</div></div>');
 		$('#incidents').append('<div id="incident-'+counter+'" class="incident row hidden" style="margin-bottom:10px"> <div class="col-md-12" style="margin-top:15px; margin-bottom:15px;"> <div class="i-date">'+this.gsx$yearofincident.$t+'</div><div class="i-area">'+this.gsx$place.$t+'</div></div><div class="col-md-12"> <div class="i-row"> <div class="i-img"><img src="assets/img/victim-icon.png"/></div><div class="i-text"> <div class="i-tag">VICTIMS</div><div class="i-desc">'+this.gsx$victim.$t+'</div></div></div><div class="i-row"> <div class="i-img"><img src="assets/img/perpatrator-icon.png"/></div><div class="i-text"> <div class="i-tag">PERPETRATOR</div><div class="i-desc">'+this.gsx$perpetrator.$t+'</div></div></div><div class="i-row"> <div class="i-img"><img src="assets/img/crime-icon.png"/></div><div class="i-text"> <div class="i-tag">CRIME</div><div class="i-desc">'+this.gsx$crime.$t+'</div></div></div><div class="i-row"> <div class="i-img"><img src="assets/img/verdict-icon.png"/></div><div class="i-text"> <div class="i-tag">VERDICT</div><div class="i-desc">'+this.gsx$verdict.$t+'</div></div></div><div class="i-row"> <div class="i-img"><img src="assets/img/sentence-icon.png"/></div><div class="i-text"> <div class="i-tag">SENTENCE</div><div class="i-desc">'+this.gsx$sentence.$t+'</div></div></div></div></div>');
 		counter++;
@@ -63,15 +63,36 @@ $.getJSON(legislation_endpoint, function(data) {
 
 	counter = 1;
 
+	//console.log(data.feed.entry);
+	var timeline_events = [];
+
 	$(data.feed.entry).each(function(){
 
 		year = this.gsx$yearofpromulgation.$t;
-		year = year[0] + year[1] + '<br>' + year[2] + year[3];
+		year = year[0] + year[1] + year[2] + year[3];
 
-		$('#legi-con').append('<div id="l-anc-'+counter+'" class="col-xs-3 col-md-2 date" div-toggle="#legislation-'+counter+'"><div>'+year+'</div></div>');
-		$('#legislations').append('<div id="legislation-'+counter+'" class="legislation row hidden" style="margin-bottom:10px"> <div class="col-md-12"> <div class="l-row" style="border-top:1px solid #B70000; margin-top:20px;"> <div class="l-img"><img src="assets/img/law-icon.png"/></div><div class="l-text"> <div class="l-tag">LAW</div><div class="l-desc">'+this.gsx$law.$t+'</div></div></div><div class="l-row"> <div class="l-tag">REGIME</div><div class="l-desc">'+this.gsx$regime.$t+'</div></div><div class="l-row"> <div class="l-tag">KEY FEATURES</div><div class="l-desc">'+this.gsx$keyfeatures.$t+'</div></div></div></div>');
-		counter++;
+		timeline_events.push({
+	        "media": {
+	          "url": "",
+	        },
+	        "start_date": {
+	          "year": year
+	        },
+	        "text": {
+	        	"headline": this.gsx$law.$t,
+	          	"text": '<div id="legislation-'+counter+'" class="legislation row" style="margin-bottom:10px"> <div class="col-md-12"> <div class="l-row" style="border-top:1px solid #B70000; margin-top:20px;"> <div class="l-img"><img src="assets/img/law-icon.png"/></div><div class="l-text"> <div class="l-tag">LAW</div><div class="l-desc">'+this.gsx$law.$t+'</div></div></div><div class="l-row"> <div class="l-tag">REGIME</div><div class="l-desc">'+this.gsx$regime.$t+'</div></div><div class="l-row"> <div class="l-tag">KEY FEATURES</div><div class="l-desc">'+this.gsx$keyfeatures.$t+'</div></div></div></div>'
+	        }
+	      });
+		
+
+		//$('#legi-con').append('<div id="l-anc-'+counter+'" class="col-xs-3 col-md-2 date" div-toggle="#legislation-'+counter+'"><div>'+year+'</div></div>');
+		//$('#legislations').append('<div id="legislation-'+counter+'" class="legislation row hidden" style="margin-bottom:10px"> <div class="col-md-12"> <div class="l-row" style="border-top:1px solid #B70000; margin-top:20px;"> <div class="l-img"><img src="assets/img/law-icon.png"/></div><div class="l-text"> <div class="l-tag">LAW</div><div class="l-desc">'+this.gsx$law.$t+'</div></div></div><div class="l-row"> <div class="l-tag">REGIME</div><div class="l-desc">'+this.gsx$regime.$t+'</div></div><div class="l-row"> <div class="l-tag">KEY FEATURES</div><div class="l-desc">'+this.gsx$keyfeatures.$t+'</div></div></div></div>');
+		//counter++;
 	});
+
+	var timeline_data = {events:timeline_events};
+	var timeline = new TL.Timeline('timeline-embed', timeline_data);
+	//console.log(timeline_events);
 
 	console.log('legislation populated');
 
@@ -96,6 +117,10 @@ $.getJSON(legislation_endpoint, function(data) {
 	});
 
 });
+
+
+
+
 
 
 
